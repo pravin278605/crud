289f7fa4f5ff0dda7467dcec0155f8a5f9b33b75
pravin278605 <pravin.patil2786@gmail.com>
2026-02-08 11:16:46 +0530

    file changes

--- backend/middleware/loginValidation.js
+++ backend/middleware/loginValidation.js
@@ -1,10 +1,12 @@
 const { body } = require("express-validator");
 exports.loginValidation = [
   body("email")
-    .notEmpty().withMessage("Email is required")
-    .isEmail().withMessage("Invalid email"),
+    .notEmpty().withMessage("Enter Email Id")
+    .bail()      
+    .isEmail().withMessage("Enter valid email."),
 
   body("password")
-    .notEmpty().withMessage("Password is required")
+    .notEmpty().withMessage("Enter the Password.")
+    .bail()
     .isLength({ min: 5 }).withMessage("Password must be at least 5 chars")
 ];
